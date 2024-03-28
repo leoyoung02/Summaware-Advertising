2438c6551125a160378e14175e6b6f8bbf521a8a
leoyoung02 <flarelight0125@gmail.com>
2024-03-28 08:46:07 -0700

    Initialization commit

---
+++ Media_Manager/apps/Advertising/views/admin.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+
+def admin(request):
+	# Check if user is logged in, if not, redirect  to login screen
+	if request is None or not request.user.is_authenticated:
+		return redirect(login_redirect + '/')
+	return render(request, 'admin/admin.html')
+
+def adminGeneral(request):
+	# Check if user is logged in, if not, redirect  to login screen
+	if request is None or not request.user.is_authenticated:
+		return redirect(login_redirect + '/')
+	return render(request, 'admin/admin-general.html')
+
+def adminAds(request):
+	# Check if user is logged in, if not, redirect  to login screen
+	if request is None or not request.user.is_authenticated:
+		return redirect(login_redirect + '/')
+	return render(request, 'admin/ads/ads.html')
+
+def adminFinancial(request):
+	# Check if user is logged in, if not, redirect  to login screen
+	if request is None or not request.user.is_authenticated:
+		return redirect(login_redirect + '/')
+	return render(request, 'admin/financial/admin-financial.html')
+
+def adminFinancialFiscal(request):
+	# Check if user is logged in, if not, redirect  to login screen
+	if request is None or not request.user.is_authenticated:
+		return redirect(login_redirect + '/')
+	return render(request, 'admin/financial/fiscal-year.html')
+
+def adminNewMagazine(request):
+	# Check if user is logged in, if not, redirect  to login screen
+	if request is None or not request.user.is_authenticated:
+		return redirect(login_redirect + '/')
+	return render(request, 'admin/products/new-magazine.html')
