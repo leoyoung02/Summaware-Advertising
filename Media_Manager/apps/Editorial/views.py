57b05c605dfa4f908b91ec1c5553c5efde0c3052
leoyoung02 <flarelight0125@gmail.com>
2024-04-11 17:25:58 -0400

    Merging

--- Media_Manager/apps/Editorial/views.py
+++ Media_Manager/apps/Editorial/views.py
@@ -11,14 +11,7 @@ def editorial_view(request):
 		return redirect(login_redirect + "editorial")
 
 	if not request.user.has_perm('BI.editorial_access'):
-<<<<<<< HEAD
-		return render(request, "coming_soon.html", {"access": "deny", "message": "Access denied!", "menu": views.get_sidebar(request)})
-
-	context = {"access": "allow", "message":"", "groups": ', '.join(views.get_groups(request)), "menu": views.get_sidebar(request)}
-	return render(request, "coming_soon.html", context)
-=======
 		return render(request, "editorial.html", {"access": "deny", "message": "Access denied!", "menu": views.get_sidebar(request)})
 
 	context = {"access": "allow", "message":"", "groups": ', '.join(views.get_groups(request)), "menu": views.get_sidebar(request)}
 	return render(request, "editorial.html", context)
->>>>>>> 7a937277d63f6c5c5670567696935b34e97526a0
--- Media_Manager/apps/Production/views.py
+++ Media_Manager/apps/Production/views.py
@@ -11,14 +11,7 @@ def production_view(request):
 		return redirect(login_redirect + "production")
 
 	if not request.user.has_perm('BI.production_access'):
-<<<<<<< HEAD
-		return render(request, "coming_soon.html", {"access": "deny", "message": "Access denied!", "menu": views.get_sidebar(request)})
-
-	context = {"access": "allow", "message":"", "groups": ', '.join(views.get_groups(request)), "menu": views.get_sidebar(request)}
-	return render(request, "coming_soon.html", context)
-=======
 		return render(request, "production.html", {"access": "deny", "message": "Access denied!", "menu": views.get_sidebar(request)})
 
 	context = {"access": "allow", "message":"", "groups": ', '.join(views.get_groups(request)), "menu": views.get_sidebar(request)}
 	return render(request, "production.html", context)
->>>>>>> 7a937277d63f6c5c5670567696935b34e97526a0
