50e7aed3325d2e9ed62c4513fa2501db4a13d327
leoyoung02 <flarelight0125@gmail.com>
2024-07-05 16:09:28 +0300

    classfied

--- static/dist/js/advertising/ClassfiedsNew.js
+++ static/dist/js/advertising/ClassfiedsNew.js
@@ -945,14 +945,14 @@ function filterAdFormats() {
     adFormatEle.innerHTML = adFormatHtml;
 }
 
-const getCookie = name => {
-    let cookie = {};
-    document.cookie.split(';').forEach(function (el) {
-        let [k, v] = el.split('=');
-        cookie[k.trim()] = v;
-    })
-    return cookie[name];
-}
+// const getCookie = name => {
+//     let cookie = {};
+//     document.cookie.split(';').forEach(function (el) {
+//         let [k, v] = el.split('=');
+//         cookie[k.trim()] = v;
+//     })
+//     return cookie[name];
+// }
 
 function getCampaignDetail() {
     let campaignDetailEle = document.getElementById("sum-ad-details");
